a95577d4e1c2585daa5477d69b01c25f11d6dbe5
princecuberdon <princecuberdon@bandcochon.fr>
2016-02-07 20:16:54 +0100

    Make the tests passing following the Django 1.8 passage and a small refactorisation

--- tests.py
+++ tests.py
@@ -1,19 +1,7 @@
 # -*- coding: UTF-8 -*-
 # capatcha is part of Band Cochon
 # Band Cochon (c) Prince Cuberdon 2011 and Later <princecuberdon@bandcochon.fr>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import os
 
@@ -27,21 +15,22 @@ from django.conf import settings
 from .models import Preference
 from .templatetags.capatcha_tags import Capatcha
 
+
 class TestCapatcha(TestCase):
     def setUp(self):
         """ Create a dummy capatcha 
-        FIXME: Move to MEDIA_URL + CAPATCHA_CONFIG.Download image and font from test_bc/resources/
+        FIXME: Move to MEDIA_URL + CAPATCHA_CONFIG.Download image and font from tests/resources/
         """
         self.cap = Preference.objects.create(
             font="capatcha/ACIDIC.ttf",
             background="capatcha/antispam.png"
         )
-        
+
     def test_tag(self):
         """ Test the template tag. 
         Our user have no sessionkey so the sessionkey is None, the image is called None.png 
         """
-        
+
         tpl = u'''{% load capatcha_tags %}{% create_capatcha %}{{ capatcha.path }}'''
         # Simulate a request
         req = HttpRequest()
@@ -50,10 +39,9 @@ class TestCapatcha(TestCase):
         result = Template(tpl).render(RequestContext(req))
         self.assertEqual(result, os.path.join(settings.MEDIA_URL, settings.CAPATCHA_CONFIG.temp, u"None.png"))
 
-    
     def test_capatcha_object_remove_old(self):
         """ this can't be tested here due to Unix mecanism about creation date """
-        
+
     def test_capatcha_object_is_valid(self):
         """ Test key validity """
         req = HttpRequest()
@@ -61,5 +49,3 @@ class TestCapatcha(TestCase):
         req.session = SessionBase()
         cap = Capatcha(req)
         self.assertFalse(cap.isValid("acodetoolongtobevalid"))
-        
-        
